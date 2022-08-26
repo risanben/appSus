29804a5293495697249a20bad37986f8c2b0f9e2
risanBenichou <ris.benichou@gmail.com>
2022-08-26 17:00:14 +0300

    2.2 about beginning

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,22 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <div className="about-header">
+            <h2>Who We Are</h2>
+            <div className="about-header-bottom">
+                <h4>App Developers - short insight</h4>
+                <ul>
+                    <li>home</li>
+                    <li>resume</li>
+                    <li>projects</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="about-main-content">
+            <div className="about-left"></div>
+            <div className="about-right"></div>
+        </div>
+        
     </section>
 }
